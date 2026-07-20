932c5913601447d1e39c22167ad43dd3030b72c2
cajetan77 <caje.pup.trindade@gmail.com>
2026-07-21 09:17:52 +1200

    Update Grant-EntraAppSitePermission.ps1 to specify site URL and app credentials. Added connection to Microsoft Graph with tenant ID and client secret, and included functionality to retrieve site context. Improved script structure for better clarity and functionality.

--- Reporting/Grant-EntraAppSitePermission.ps1
+++ Reporting/Grant-EntraAppSitePermission.ps1
@@ -3,16 +3,16 @@
     Grants an Entra app Write permission to a SharePoint site.
 #>
 Param(
-    [string]$SiteUrl = "https://sharepoint.com",
-    [string]$AppId = "YOUR-APP-CLIENT-ID",
-    [string]$AppDisplayName = "Your App Name",
+    [string]$SiteUrl = "https://caje77sharepoint.sharepoint.com/sites/WRK-AboutUs",
+    [string]$AppId = "519ad27a-1af4-41e7-87e0-825fb8e368d6",
+    [string]$AppDisplayName = "TestSiteSelected",
     [ValidateSet('Read', 'Write')]
     [string]$Permissions = "Write"
 )
 
 Import-Module PnP.PowerShell
 
-Connect-PnPOnline -Url $SiteUrl -Interactive
+Connect-PnPOnline -Url $SiteUrl -Interactive -ClientId 66a1852a-1f21-46a2-ad58-35fc4c3f1530
 
 Grant-PnPAzureADAppSitePermission `
     -AppId $AppId `
@@ -22,4 +22,21 @@ Grant-PnPAzureADAppSitePermission `
 
 Write-Host "Granted $Permissions to '$AppDisplayName' on $SiteUrl"
 
+
+
+
 Disconnect-PnPOnline
+
+$TenantId = "764b46e8-d798-4ed3-87db-ae55ed7b0432"
+$clientsecret = ""
+$secureSecret = ConvertTo-SecureString -String $clientsecret -AsPlainText -Force
+$credential = [PSCredential]::new($appid, $secureSecret)
+Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $credential -NoWelcome
+
+#Connect-MgSite -SiteId $SiteUrl
+
+Get-MgContext
+
+$TargetSite = Get-MgSite -SiteId "caje77sharepoint.sharepoint.com:/sites/WRK-AboutUs"
+
+Disconnect-MgGraph
